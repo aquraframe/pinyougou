22883092376578ea60ad0aef9db60719d160e3a6
kj <kj@163.com>
2019-01-22 13:31:08 +0800

    搜索获取品牌

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -1,20 +1,32 @@
 package com.pinyougou.search.service.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.GroupOptions;
+import org.springframework.data.solr.core.query.HighlightOptions;
+import org.springframework.data.solr.core.query.HighlightQuery;
 import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleHighlightQuery;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.GroupEntry;
+import org.springframework.data.solr.core.query.result.GroupPage;
+import org.springframework.data.solr.core.query.result.GroupResult;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 
-@Service(timeout=5000)
+@Service(timeout=10000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 
 	@Autowired
@@ -23,12 +35,62 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 	@Override
 	public Map<String, Object> search(Map searchMap) {
 		Map<String, Object> map = new HashMap<>();
-		Query query = new SimpleQuery();
-		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
-		query.addCriteria(criteria);
-		ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-		map.put("rows", page.getContent());
+//		Query query = new SimpleQuery("*:*");
+//		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+//		query.addCriteria(criteria);
+//		ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+//		System.out.println(page);
+//		map.put("rows", page.getContent());
+		map.putAll(searchList(searchMap));
+		List categoryList = searchCategoryList(searchMap);
+		map.put("categoryList", categoryList);
 		return map;
 	}
-
+	
+	private Map searchList(Map searchMap) {
+		Map map = new HashMap();
+		HighlightQuery query = new SimpleHighlightQuery();
+		HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");
+		highlightOptions.setSimplePrefix("<em style='color:red'>");
+		highlightOptions.setSimplePostfix("</em>");
+		query.setHighlightOptions(highlightOptions);
+		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+		query.addCriteria(criteria);
+		HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+		for (HighlightEntry<TbItem>	entry : page.getHighlighted()) {
+			TbItem item = entry.getEntity();
+			if (entry.getHighlights().size()>0 && entry.getHighlights().get(0).getSnipplets().size()>0) {
+				item.setTitle(entry.getHighlights().get(0).getSnipplets().get(0));
+			}
+		}
+		map.put("rows", page.getContent());		
+		return map;
+	}
+	
+	private List searchCategoryList(Map searchMap) {
+		List<String> list = new ArrayList<>();
+		Query query = new SimpleQuery("*:*");
+		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+		query.addCriteria(criteria);
+		GroupOptions groupOptions = new GroupOptions().addGroupByField("item_category");
+		query.setGroupOptions(groupOptions);
+		GroupPage<TbItem> page =solrTemplate.queryForGroupPage(query, TbItem.class);
+		GroupResult<TbItem> groupResult = page.getGroupResult("item_category");
+		Page<GroupEntry<TbItem>> groupEntries = groupResult.getGroupEntries();
+		List<GroupEntry<TbItem>> content = groupEntries.getContent();
+		for (GroupEntry<TbItem> entry : content) {
+			list.add(entry.getGroupValue());
+		}
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
